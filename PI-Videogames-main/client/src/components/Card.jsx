6f28f8b7f3be190bbd7671dadbc761acdd6d28aa
Teche95 <silencertec@gmail.com>
2022-08-09 17:25:22 -0300

    prueba

--- PI-Videogames-main/client/src/components/Card.jsx
+++ PI-Videogames-main/client/src/components/Card.jsx
@@ -4,16 +4,14 @@ import styles from "./Card.module.css"
 // import Error from "./Error";
 
 const Card = ({ image, name, genres, id }) => {
-    // console.log(genres)
+    console.log(genres)
     return (
 
 
         <div className={styles.wrapper} >
             <Link to={`/home/${id}`}>
                 {<h3 >{name}</h3>}
-
                 <img src={image} alt='img not found' width='350px' height='200px' />
-
             </Link>
             {genres && genres.map((e, i) =>
                 <h4 key={i} >{e}</h4>
